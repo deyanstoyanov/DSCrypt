2d5f16941b4f333094480cba79a9c01112433339
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2017-03-05 13:11:05 +0200

    Implemented ExitCommand in Shell

--- DSCrypt/DSCrypt/Shell/ShellViewModel.cs
+++ DSCrypt/DSCrypt/Shell/ShellViewModel.cs
@@ -1,6 +1,22 @@
 ﻿namespace DSCrypt.Shell
 {
+    using System.Windows;
+    using System.Windows.Input;
+
+    using Microsoft.Practices.Prism.Commands;
+
     public class ShellViewModel
     {
+        public ShellViewModel()
+        {
+            this.ExitCommand = new DelegateCommand(this.OnExitCommand);
+        }
+
+        public ICommand ExitCommand { get; set; }
+
+        private void OnExitCommand()
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
