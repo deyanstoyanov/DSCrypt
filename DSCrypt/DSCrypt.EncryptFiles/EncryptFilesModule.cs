47897fd034bbf24da3c7f8f3de9bba37aaeafaa6
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2017-03-05 15:21:49 +0200

    Created EncryptFilesModule
    Installed CommonServiceLocator v.1.2
    Installed Prism.Composition v.5.0.0
    Installed Prism.Mvvm v.1.0.0

---
+++ DSCrypt/DSCrypt.EncryptFiles/EncryptFilesModule.cs
@@ -0,0 +1,14 @@
+﻿namespace DSCrypt.EncryptFiles
+{
+    using System;
+
+    using Microsoft.Practices.Prism.Modularity;
+
+    public class EncryptFilesModule : IModule
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
