386c3ec9cddbeddc7493bdf90d4970232122fb8d
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2017-03-05 12:19:49 +0200

    Using statements sorted and moved inside namespace declaration
    Code refactored

--- DSCrypt/DSCrypt/Shell/Shell.xaml.cs
+++ DSCrypt/DSCrypt/Shell/Shell.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-namespace DSCrypt.Shell
+﻿namespace DSCrypt.Shell
 {
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for Shell.xaml
     /// </summary>
@@ -21,7 +9,7 @@ namespace DSCrypt.Shell
     {
         public Shell()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
