0c29376071ce6f8784a6e077091ca0e16a0e5336
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2017-03-05 12:57:21 +0200

    Integrated a basic bootstrapping
    Created DsCryptBootstrapper

--- DSCrypt/DSCrypt/App.xaml.cs
+++ DSCrypt/DSCrypt/App.xaml.cs
@@ -2,10 +2,19 @@
 {
     using System.Windows;
 
+    using DSCrypt.Bootstrapper;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DsCryptBootstrapper bootstrapper = new DsCryptBootstrapper();
+            bootstrapper.Run();
+        }
     }
 }
---
+++ DSCrypt/DSCrypt/Bootstrapper/DsCryptBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿namespace DSCrypt.Bootstrapper
+{
+    using System.Windows;
+
+    using DSCrypt.Shell;
+
+    using Microsoft.Practices.Prism.UnityExtensions;
+    using Microsoft.Practices.Unity;
+
+    public class DsCryptBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (Shell)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
